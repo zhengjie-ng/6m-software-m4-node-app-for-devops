2d27fbe930f331d2219a405c8d1b37e9808540eb
Ng Zheng Jie <zhengjie.zjie@gmail.com>
2025-08-28 21:08:33 +0800

    pull from docker to heroku

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 // This is the only function that will be called by the endpoint.
 function print(req, res) {
-  res.send("Hello world!!");
+  res.send("Hello world!!!");
 }
 
 function home(req, res) {
